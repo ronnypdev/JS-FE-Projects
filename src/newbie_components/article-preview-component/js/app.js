ec4f9fd3fa02534405dfdb46a73bb6f66b1d14fb
Ronny Perez <ronnypdev@gmail.com>
2023-11-13 18:51:05 -0500

    adding js on going

---
+++ src/newbie_components/article-preview-component/js/app.js
@@ -0,0 +1,8 @@
+const shareButton = document.querySelector('.profile-share-button');
+const shareSVG = document.querySelector('.profile-share-arrow');
+
+shareButton.addEventListener('click', function () {
+  console.log('click');
+  // this.classList.toggle('js-profile-button');
+  // shareSVG.style.fill = '#ffffff';
+})
