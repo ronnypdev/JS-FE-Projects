19403007298c101960b318a986762409f087ed61
Ronny Perez <ronnypdev@gmail.com>
2023-11-14 13:10:41 -0500

    FM-3 implemented share button color change

--- src/newbie_components/article-preview-component/js/app.js
+++ src/newbie_components/article-preview-component/js/app.js
@@ -1,8 +1,12 @@
 const shareButton = document.querySelector('.profile-share-button');
+console.log("shareButton: ", shareButton);
 const shareSVG = document.querySelector('.profile-share-arrow');
+console.log("shareSVG: ", shareSVG);
+
+
 
 shareButton.addEventListener('click', function () {
-  console.log('click');
-  // this.classList.toggle('js-profile-button');
-  // shareSVG.style.fill = '#ffffff';
+  console.log('click is working');
+  this.classList.toggle('js-profile-button');
+  shareSVG.classList.toggle('fill-white');
 })
