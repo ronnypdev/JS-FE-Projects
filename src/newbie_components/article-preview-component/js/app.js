5fce081a8a2e3ee9884f2b7a9cd72e8903d23b0f
Ronny Perez <ronnypdev@gmail.com>
2023-11-17 20:31:07 -0500

    FM-3 implemented tooltip and javascript functionality

--- src/newbie_components/article-preview-component/js/app.js
+++ src/newbie_components/article-preview-component/js/app.js
@@ -2,6 +2,7 @@ const shareButton = document.querySelector('.profile-share-button');
 console.log("shareButton: ", shareButton);
 const shareSVG = document.querySelector('.profile-share-arrow');
 console.log("shareSVG: ", shareSVG);
+const modalOpen = document.querySelector('.share-modal');
 
 
 
@@ -9,4 +10,8 @@ shareButton.addEventListener('click', function () {
   console.log('click is working');
   this.classList.toggle('js-profile-button');
   shareSVG.classList.toggle('fill-white');
+
+  if (window.innerWidth > 768) {
+    modalOpen.classList.toggle('dk-modal-open');
+  }
 })
